4f91a8f13e8ea09901a8491161f6abe6d2cc3a4f
fishyfishfish55 <l.lacossct@gmail.com>
2025-03-06 13:55:01 -0500

    stick with tmux for now

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,24 +50,23 @@ return {
         }
     end
   },
-  { 'mrjones2014/smart-splits.nvim', build = './kitty/install-kittens.bash' },
---   { 'alexghergh/nvim-tmux-navigation', config = function()
---   
---       local nvim_tmux_nav = require('nvim-tmux-navigation')
---   
---       nvim_tmux_nav.setup {
---           disable_when_zoomed = true -- defaults to false
---       }
---   
---       vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
---       vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
---       vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
---       vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
---       vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
---       vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
---   
---   end
---   },
+  { 'alexghergh/nvim-tmux-navigation', config = function()
+  
+      local nvim_tmux_nav = require('nvim-tmux-navigation')
+  
+      nvim_tmux_nav.setup {
+          disable_when_zoomed = true -- defaults to false
+      }
+  
+      vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+      vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  
+  end
+  },
   {
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = {'nvim-lua/plenary.nvim'},
